1aea7a02a4de993fed605f0f98ebd2fb0783320c
OskarGrzelak <oskargrzelak@gmail.com>
2018-08-01 12:39:01 +0200

    index.js update - wish list functionality

--- src/js/index.js
+++ src/js/index.js
@@ -12,7 +12,8 @@ const elements = {
     searchList: document.getElementById('search-list'),
     addToLib: document.getElementsByClassName('results__action--add'),
     delFromLib: document.getElementsByClassName('book__action--del'),
-    libraryList: document.getElementsByClassName('books-panel__list')[0]
+    libraryList: document.getElementsByClassName('books-panel__list')[0],
+    wishList: document.getElementsByClassName('wish-panel__list')[0]
 };
 
 // SEARCH PANEL
@@ -85,11 +86,11 @@ const renderResult = (result) => {
         <li>
             <div class="results__item" id="${result.id}">
                 <h3 class="results__title">${limitTitle(result.volumeInfo.title)}</h3>
-                <p class="results__author">${result.volumeInfo.authors[0]}</p>
+                <p class="results__author">${result.volumeInfo.authors}</p>
                     <div class="results__actions">
                         <ul>
                             <li class="results__action results__action--add">Add to lib</li>
-                            <li class="results__action">Add as wish</li>
+                            <li class="results__action results__action--wish">Add as wish</li>
                         </ul>
                     </div>
                 </div>
@@ -170,16 +171,70 @@ class Library {
                 </div>
             </li>
             `;
-            elements.libraryList.insertAdjacentHTML('beforeend', markup);
+            elements.libraryList.insertAdjacentHTML('afterbegin', markup);
         });
     }
 }
 
 const clearLibrary = () => elements.libraryList.innerHTML = '';
 
+// WISH LIST
+class WishList {
+    constructor() {
+        this.wishes = [];
+        this.num = 0;
+    }
+
+    addNewBook(id) {
+        this.wishes.push(new Book(id));
+    }
+
+    moveBook(id) {
+        const index = this.wishes.findIndex(el => el.id === id);
+        state.library.books.push(this.wishes[index]);
+        this.deleteBook(id);
+    }
+
+    deleteBook(id) {
+        const index = this.wishes.findIndex(el => el.id === id);
+        if(this.wishes.length === 1 || index === this.wishes.length-1) {
+            this.wishes.pop();
+        } else {
+            for (let i = index; i < this.wishes.length-1; i++) {
+                this.wishes[i] = this.wishes[i+1];
+            }
+            this.wishes.pop();
+        }
+        this.num--;
+    }
+
+    renderWishList(wishes) {
+        wishes.forEach(wish => {
+            const markup = `
+            <li>
+                <div class="wish" id="${wish.id}">
+                    <h3 class="wish__title">${wish.title}</h3>
+                    <p class="wish__author">${wish.author}</p>
+                    <div class="wish__actions">
+                        <ul>
+                            <li class="wish__action wish__action--add">Add</li>
+                            <li class="wish__action wish__action--del">Del</li>
+                        </ul>
+                    </div>
+                </div>
+            </li>
+            `;
+            elements.wishList.insertAdjacentHTML('afterbegin', markup);
+        });
+    }
+}
+
+const clearWishList = () => elements.wishList.innerHTML = '';
+
 // CONTROLLER
 const state = {};
 state.library = new Library();
+state.wishList = new WishList();
 
 //control search
 const controlSearch = async () => {
@@ -245,4 +300,53 @@ elements.libraryList.addEventListener('click', e => {
         state.library.renderLibrary(state.library.books);
     }
 });
+
+// control wish list
+const addBookToWishList = async (id) => {
+    // create new book object
+    state.wishList.addNewBook(id);
+
+    try {
+        await state.wishList.wishes[state.wishList.num].getBook();
+        state.wishList.num++;
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+elements.searchList.addEventListener('click', async e => {
+    const wish = e.target.closest('.results__action--wish');
+    if(wish) {
+        elements.searchPanel.classList.remove('search__visible');
+        setTimeout(clearResults, 400);
+        const id = e.target.parentElement.parentElement.parentElement.id;
+        await addBookToWishList(id);
+        clearWishList();
+        state.wishList.renderWishList(state.wishList.wishes);
+    }
+});
+
+elements.wishList.addEventListener('click', e => {
+    const add = e.target.closest('.wish__action--add');
+    if(add) {
+        const id = e.target.parentElement.parentElement.parentElement.id;
+        state.wishList.moveBook(id);
+        clearWishList();
+        clearLibrary();
+        state.wishList.renderWishList(state.wishList.wishes);
+        state.library.renderLibrary(state.library.books);
+    }
+});
+
+elements.wishList.addEventListener('click', e => {
+    const del = e.target.closest('.wish__action--del');
+    if(del) {
+        const id = e.target.parentElement.parentElement.parentElement.id;
+        state.wishList.deleteBook(id);
+        clearWishList();
+        state.wishList.renderWishList(state.wishList.wishes);
+    }
+});
+
+
 
